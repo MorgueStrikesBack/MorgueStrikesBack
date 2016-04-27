45bee1a696f67e290582d1c096c9b1f1cd1ae74e
EternalReaper <henry.sandersberg@yahoo.com.au>
2016-04-27 20:46:16 +1000

    Added enemy animations
    currently unused

--- enemy.js
+++ enemy.js
@@ -1,10 +1,32 @@
+var ANIM_MOVING = 0;
+var ANIM_DYING = 1;
+
+var ANIM_MAX = 2;
 
 var Enemy = function() {
-	this.image = document.createElement("img");
-	this.position = new Vector2(58,58)
+	this.sprite = new Sprite("BlueSpikey.png");
+	
+	this.sprite.buildAnimation(12, 1, 64, 56, 0.05,
+		[0, 1, 2, 3]);
+	
+	this.sprite.buildAnimation(12, 1, 64, 56, 0.05,
+		[4, 5, 6, 7, 8, 9, 10, 11]);
+
+
+	
+
+	
+	for(var i=0; i<ANIM_MAX; i++)
+	{
+		this.sprite.setAnimationOffset(i, -55, -87);
+	}
+
+	this.position = new Vector2(58, 58)
+
+	this.velocity = new Vector2(0,0)
+	
 	this.width = 64;
 	this.height = 56;
-	this.image.src = "Enemy.png"; 
 };
 
 Enemy.prototype.update = function(deltaTime)
