95f00bf678533cee79d165b656778f040a96081a
TheInfiniteDevil8666 <Thomas-Murray@users.noreply.github.com>
2016-06-15 18:47:09 +0800

    moving player

--- Player.js
+++ Player.js
@@ -7,23 +7,27 @@ var ANIM_IDLE_LEFT = 0;
 var ANIM_JUMP_LEFT = 1;
 var ANIM_WALK_LEFT = 2;
 var ANIM_SHOOT_LEFT = 3;
-var ANIM_CLIMB = 4;
+var ANIM_MELEE_LEFT = 4;
 var ANIM_IDLE_RIGHT = 5;
 var ANIM_JUMP_RIGHT = 6;
 var ANIM_WALK_RIGHT = 7;
 var ANIM_SHOOT_RIGHT = 8;
-var ANIM_MAX = 9;
+var ANIM_MELEE_RIGHT = 9;
+var ANIM_MAX = 10;
 
 var Player = function() {
     this.sprite = new Sprite("player.png");
-    this.sprite.buildAnimation(23, 1,40,46, 1, []);
-    this.sprite.buildAnimation(23, 1,40,46, 1, [4,5,6,7,]);
-    this.sprite.buildAnimation(23, 1,40,46, 1, []);
-    this.sprite.buildAnimation(23, 1,40,46, 1, []);
-    this.sprite.buildAnimation(23, 1,40,46, 1, []);
-    this.sprite.buildAnimation(23, 1,40,46, 1, [8,9,10,11]);
-    this.sprite.buildAnimation(23, 1,40,46, 1, []);
-    this.sprite.buildAnimation(23, 1,40,46, 1, []);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [0, 1, 2, 3,]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, []);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [4,5,6,7,]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [16,17,18]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [8,9,10,11,12,13,14,15]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [19,20,21,22]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, []);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [23,24,25,26]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [35,36,37]);
+    this.sprite.buildAnimation(19, 2, 40, 49, 0.3, [27,28,29,30,31,32,33,34]);
+    
 
     for (var i = 0; i < ANIM_MAX; i++) {
         this.sprite.setAnimationOffset(i, -55, -87);
@@ -32,15 +36,10 @@ var Player = function() {
     this.position = new Vector2();
     this.position.set = (9 * TILE, 0 * TILE);
 
-    this.sprite.x = this.position.x
-    this.sprite.y = this.position.y
-
     this.width = 15;
     this.height = 40;
 
     this.velocity = new Vector2();
-
-    this.velocity.set(0, 0);
 
     this.falling = true;
     this.jumping = false;
@@ -84,7 +83,7 @@ Player.prototype.update = function(deltaTime) {
                     this.sprite.setAnimation(ANIM_IDLE_RIGHT);
                 }
 
-            }
+            } 
         }
     }
     if (keyboard.isKeyDown(keyboard.KEY_UP) == true) {
@@ -96,19 +95,6 @@ Player.prototype.update = function(deltaTime) {
             this.sprite.setAnimation(ANIM_JUMP_RIGHT);
         }
         
-        sfxJump.play();
-        this.cooldownTime = 1;
-    }
-    
-    if(this.cooldownTimer > 0)
-    {
-        this.cooldownTimer -= deltaTime;
-    }
-    if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true && this.cooldownTimer <=0) 
-    {
-        
-        sfxFire.play();
-        this.cooldownTimer = 0.3;
     }
 
     var wasleft = this.velocity.x < 0;
@@ -120,7 +106,7 @@ Player.prototype.update = function(deltaTime) {
     if (left)
         ddx = ddx - ACCEL;
     else if (wasleft)
-        ddx = ddx + FRICTION;
+        ddx = ddx + FRICTION; 
 
     if (right)
         ddx = ddx + ACCEL;
@@ -192,20 +178,8 @@ Player.prototype.update = function(deltaTime) {
         }
 
     }
-
-    if (this.direction == LEFT)
-    {
-        this.sprite.x = this.position.x - 20
-        this.sprite.y = this.position.y
-    }
-    else 
-    {
-        this.sprite.x = this.position.x
-        this.sprite.y = this.position.y
-    }
-
 }
 
 Player.prototype.draw = function() {
-    this.sprite.draw(context, this.sprite.x, this.sprite.y);
-}
+    this.sprite.draw(context, this.position.x, this.position.y);
+} 
