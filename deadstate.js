a65f6049afdd5308854010871e1bfcda95ccc58c
person2314 <greencaleb@hotmail.com>
2016-06-29 19:31:55 +0930

    DeadState

---
+++ deadstate.js
@@ -0,0 +1,21 @@
+
+var DeadState = function() 
+{
+	this.prototype = BaseState;
+}
+
+DeadState.prototype.load = function() 
+{
+}
+
+DeadState.prototype.unload = function() 
+{
+}
+
+DeadState.prototype.update = function(dt) 
+{
+}
+
+DeadState.prototype.draw = function() 
+{
+}
